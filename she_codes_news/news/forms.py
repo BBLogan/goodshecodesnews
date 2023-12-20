13f8a600e5982fdcf0e7c43888591b77c2f29b58
BexLogan <bek.jayne@gmail.com>
2023-12-20 22:30:40 +0800

    LOVELY JUBLY! News Setup Completed & Forms Setup Completed up to and including Forms Setup Step 8: add a link on the homepage

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -1,5 +1,6 @@
 # Forms Setup Step 1: creating a forms.py file
 # Forms Setup Step 2: adding a ModelForm class
+# Forms Setup Step 7: add a widget
 
 # news/forms.py
 from django import forms
@@ -10,3 +11,4 @@ class StoryForm(ModelForm):
     class Meta: 
         model = NewsStory
         fields = ['title', 'author', 'pub_date', 'content']
+        widgets = {'pub_date': forms.DateInput(format='%m/%d/%Y', attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),}
