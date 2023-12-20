7159378c6f9c6c4226e9af5840836920f07cf6d3
BexLogan <bek.jayne@gmail.com>
2023-12-20 23:39:52 +0800

    Migrated successfully & added relationship between stories & users - completed up to & including Users Setup Step 17: add a relationship between stories and users

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,11 @@
+# Users Setup Step 16: add a relationship between stories and users
+
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
     content = models.TextField()
