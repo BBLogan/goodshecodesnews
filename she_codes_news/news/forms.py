d9175ec38a22347df515b3cf89fe6a4408d7f88f
BexLogan <bek.jayne@gmail.com>
2023-12-22 13:29:04 +0800

    now the work begins - satisfying assignment requirements

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -7,9 +7,11 @@
 from django import forms
 from django.forms import ModelForm
 from .models import NewsStory
+from django.template.defaultfilters import mark_safe
 
 class StoryForm(ModelForm):
     class Meta: 
         model = NewsStory
         fields = ['title', 'pub_date', 'content']
         widgets = {'pub_date': forms.DateInput(format='%m/%d/%Y', attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),}
+        # label = mark_safe()
